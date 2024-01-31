b06632dbba78173d99bd5c0646bc6b7befb8d5f9
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-01-31 15:24:46 -0300

    Inciado javascript para ocultar e add class hidden

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
-console.log("Funcionando!")
+const searchInput = document.getElementById('search__input');
+const resultArtisat = documento.getElementById('result__artist');
+const resultPlaylist = documento.getElementById('result__playlist');
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtisat.classList.remove('hidden');
+        return;
+    }
+})
